e9d0dd320b0244bc640f61e20b84335eae48bcfd
aldelucaizs <al.deluca@izs.it>
2021-07-08 16:55:14 +0200

    testing push on branch, fix on modal style

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,3 @@
-/*
- * This JavaScript doesn't do anything. The file exists just to demonstrate
- * including static assets from the HTML in themes.
- */
-
 const bodyEl = document.body;
 const html = document.documentElement;
 
